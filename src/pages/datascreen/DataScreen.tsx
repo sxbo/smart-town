1501238595922a958f82d3bbb2ec39f69cc8b038
xiaobo <henaxiaobo168@163.com>
2020-09-09 23:45:51 +0800

    数据大屏月面

--- src/pages/datascreen/DataScreen.tsx
+++ src/pages/datascreen/DataScreen.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+import '../../theme/style/datascreen/layout.scss';
+import '../../theme/style/common.scss';
 
 export default class DataScreen extends Component {
   constructor(props: any){
@@ -11,8 +13,32 @@ export default class DataScreen extends Component {
 
   render(){
     return (
-      <div>
-        数据大屏
+      <div className="data-screen">
+        <div className="card-box data-screen-wapper">
+          <div className="screen-head">
+            <div className="left-line"></div>
+            <div className="circle"></div>
+            <div className="screen-title">范家镇实时数据监控平台</div>
+            <div className="circle"></div>
+            <div className="right-line"></div>
+          </div>
+          <div className="screen-body">
+            <div className="screen-left-body">
+              <div className="screen-monitor-wapper"></div>
+              <div className="screen-green-wapper"></div>
+              <div className="screen-breed-wapper"></div>
+            </div>
+            <div className="screen-midd-body">
+              <div className="screen-map-wapper"></div>
+              <div className="screen-epidemic-wapper"></div>
+            </div>
+            <div className="screen-right-body">
+              <div className="screen-poor-wapper"></div>
+              <div className="screen-landslid-wapper"></div>
+              <div className="screen-scenic-wapper"></div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
